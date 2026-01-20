ecc0c7e24a3d49a798831be695318d69aeb625d0
bu-kolapo <kolapobukola2019@gmail.com>
2026-01-20 13:01:09 +0100

    Ignore microservice directories

--- src/main/java/com/corebanking/CorebankingApplication.java
+++ src/main/java/com/corebanking/CorebankingApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CorebankingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CorebankingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CorebankingApplication.class, args);
+    }
 
 }
